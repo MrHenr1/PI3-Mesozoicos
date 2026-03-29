758f487ff0b14131289bcad9f37e3adea70e41a2
Cauã Carvalho <caua2006carvalho@gmail.com>
2026-03-28 21:26:32 -0300

    feat(game): first-parte deliver 3

--- Form2.cs
+++ Form2.cs
@@ -11,6 +11,7 @@ namespace Mesozoicos {
 
         public Form2() {
             InitializeComponent();
+            txtIDListar.Text = idpartida;
         }
 
         public void Atualizartela() {
@@ -24,17 +25,36 @@ namespace Mesozoicos {
         }
 
         private void btnListarJogadores_Click(object sender, EventArgs e) {
-            int idpartida = int.Parse(txtIDListar.Text);
-            string returned = Jogo.ListarJogadores(idpartida);
+            // int idpartida = int.Parse(txtIDListar.Text);
+            string returned = Jogo.ListarJogadores(int.Parse(this.idpartida));
             txtBoxJogadores.Text = returned;
+
+            
         }
 
         private void btnIniciarPartida_Click(object sender, EventArgs e) {
-            lblPartidaStatus.Text = Jogo.Iniciar(this.idjogador, this.idsenha);
+            string gameReturn = Jogo.Iniciar(this.idjogador, this.idsenha);
 
+            //if(lblPartidaStatus.Text.StartsWith("ERRO"))
+            //{
+
+            //}
+
+
+            if (gameReturn.StartsWith("ERRO") )
+            {
+                MessageBox.Show(gameReturn, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string[] gamesInfos = gameReturn.Split(',');
             Form3 form3 = new Form3();
-            form3.idjogador = int.Parse(this.idpartida);
+            form3.idjogador = this.idjogador;
             form3.idsenha = this.idsenha;
+            form3.idPlayerDiceSelected = gamesInfos[0];
+            form3.diceSide = gamesInfos[1];
+            form3.idGame = this.idpartida;
+
             form3.Show();
         }
                               
